897d3f53d3c13fe0de79d105aef74faf2c9762f7
Aret <aretx100@gmail.com>
2021-12-29 13:17:49 +0100

    solving bugs

--- js/navbarSystem.js
+++ js/navbarSystem.js
@@ -39,6 +39,39 @@
 
  });
 
+/**
+ * 
+ * 
+ * Necesito poder marcar en el lugar donde estoy, como cargo de forma independiente
+ * el navbar, tendré que esperar hasta que este esté insertado en el DOM y posteriormente
+ * añadir la clase correspondiente 
+ * 
+ * 
+ **/
+
+
+const activarItemMenu = function(selector) {
+  let frecuency = 500;
+  let timeout = 10000;
+
+  var startTimeInMs = Date.now();
+
+  (function recurSearch() {
+
+    if ($('#' + selector).length) {
+      $("#" + selector).addClass("active-nav")
+      return;
+    }
+    else {
+      setTimeout(function () {
+
+        if (timeout && Date.now() - startTimeInMs > timeout)
+          return;
+        recurSearch();
+      }, frecuency);
+    }
+  })();
+}
 
 // Funciones de utilidad
 
